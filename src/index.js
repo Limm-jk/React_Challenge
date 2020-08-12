bc57e3afc608acf8401314c36754d3507c01b14a
Limm-jk <201602057@cs-cnu.org>
2020-08-12 13:09:55 +0900

    1. count
    값을 증가하고 감소시킬 수 있는 반응형 페이지

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import Count from './Count';
+// 최초의 React App
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App name="Limm_jk"/> */}
+    <Count />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
